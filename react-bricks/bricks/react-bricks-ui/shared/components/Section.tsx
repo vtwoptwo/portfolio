7c6960eff61c423bdab78c9f4e8d7dda5fc289c1
“vtuptu” <vtwop2@gmail.com>
2023-08-03 10:54:41 +0800

    first commit

---
+++ react-bricks/bricks/react-bricks-ui/shared/components/Section.tsx
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react'
+import classNames from 'classnames'
+import { bgColors } from '../../colors'
+import {
+  types,
+  useAdminContext,
+  useReactBricksContext,
+} from 'react-bricks/frontend'
+import Container from './Container'
+export type Border = 'full' | 'boxed' | 'none'
+
+interface SectionProps {
+  backgroundColor?: { color: string; className: string }
+  backgroundImage?: types.IImageSource
+  backgroundImageDark?: types.IImageSource
+  borderTop?: Border
+  borderBottom?: Border
+  className?: string
+  children?: React.ReactNode
+  noOverflowX?: boolean
+}
+
+const Section: React.FC<SectionProps> = ({
+  backgroundColor = bgColors.WHITE.value,
+  backgroundImage,
+  backgroundImageDark,
+  borderTop = 'none',
+  borderBottom = 'none',
+  className = '',
+  noOverflowX = false,
+  children,
+}) => {
+  const bgColor = backgroundColor.className
+  const { isAdmin } = useAdminContext()
+  const { isDarkColorMode, toggleColorMode } = useReactBricksContext()
+  const image = backgroundImage?.src !== undefined ? backgroundImage?.src : ''
+  const imageDark =
+    backgroundImage?.src !== undefined ? backgroundImageDark?.src : ''
+
+  const currentTheme = isAdmin
+    ? isDarkColorMode
+      ? 'dark'
+      : 'light'
+    : typeof window === 'undefined'
+    ? ''
+    : localStorage.getItem('color-mode')
+
+  const [bgStyle, setBgStyle] = useState<string>('none')
+
+  useEffect(() => {
+    currentTheme === 'light'
+      ? setBgStyle(`url(${image})`)
+      : setBgStyle(`url(${imageDark})`)
+  }, [currentTheme])
+
+  return (
+    <>
+      <section
+        style={{ backgroundImage: bgStyle }}
+        className={classNames(
+          bgColor,
+          className,
+          {
+            'overflow-x-hidden': noOverflowX,
+          },
+          `bg-no-repeat bg-cover bg-center`
+        )}
+      >
+        {borderTop !== 'none' && (
+          <Container
+            size={borderTop === 'boxed' ? 'medium' : 'full'}
+            paddingBottom="0"
+            paddingTop="0"
+          >
+            <hr className="border-black/10" />
+          </Container>
+        )}
+        {children}
+        {borderBottom !== 'none' && (
+          <Container
+            size={borderBottom === 'boxed' ? 'medium' : 'full'}
+            paddingBottom="0"
+            paddingTop="0"
+          >
+            <hr className="border-black/10" />
+          </Container>
+        )}
+      </section>
+    </>
+  )
+}
+
+export default Section

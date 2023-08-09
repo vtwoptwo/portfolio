7ae648876406c1845aca23ca6215ea43c99eedd6
“vtuptu” <vtwop2@gmail.com>
2023-08-09 17:15:25 +0800

    change watermark

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,32 @@
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
-
+import { useEffect } from 'react'
 import ReactBricksApp from '../components/ReactBricksApp'
+
 
 import '../css/styles.css'
 
 const MyApp = (props: AppProps) => {
+  useEffect(() => {
+    const observer = new MutationObserver(() => {
+      document.querySelectorAll('a').forEach(anchor => {
+        if (anchor.innerHTML.includes('Made in <span class="l">React Bricks</span>')) {
+          // Replace the innerHTML with your desired content
+          anchor.innerHTML = '<img src="https://assets.reactbricks.com/k9xUIXa4twJLb--/images/src_set/1Sm72ntcxTg54V5-76/vtwoptwo.webp" style="width: 18px; height: 18px; margin-right: 6px;"><span>made by vera</span>';
+        }
+      });
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+
+    // Cleanup function to disconnect the observer when the component unmounts
+    return () => observer.disconnect();
+  }, []); // Empty dependency array means this will run once after the initial render
+
+
   return (
     <ThemeProvider
       attribute="class"
@@ -15,7 +36,7 @@ const MyApp = (props: AppProps) => {
     >
       <ReactBricksApp {...props}></ReactBricksApp>
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default MyApp
+export default MyApp;

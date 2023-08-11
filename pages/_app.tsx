943d3b2c3abd40960bfb88c44f24c7c4fd71b679
“vtuptu” <vtwop2@gmail.com>
2023-08-11 11:58:22 +0800

    adapt to dark mode + change css of badge element

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,57 @@
-import { ThemeProvider } from 'next-themes'
+import { ThemeProvider, useTheme } from 'next-themes'
 import type { AppProps } from 'next/app'
-import { useEffect } from 'react'
+import { use, useEffect } from 'react'
 import ReactBricksApp from '../components/ReactBricksApp'
 
 
 import '../css/styles.css'
 
 const MyApp = (props: AppProps) => {
+
   useEffect(() => {
     const observer = new MutationObserver(() => {
       document.querySelectorAll('a').forEach(anchor => {
         if (anchor.innerHTML.includes('Made in <span class="l">React Bricks</span>')) {
           // Replace the innerHTML with your desired content
-          anchor.innerHTML = '<img src="https://assets.reactbricks.com/k9xUIXa4twJLb--/images/src_set/1Sm72ntcxTg54V5-76/vtwoptwo.webp" style="width: 18px; height: 18px; margin-right: 6px;"><span>made by vera</span>';
+          anchor.innerHTML = '<img src="https://assets.reactbricks.com/k9xUIXa4twJLb--/images/src_set/pmr5syjVsfVUaVX-400.webp" style=" width: 18px; height: 18px; margin-right: 6px; dark:backgroundColor-green;"><span>made by vera</span>';
           anchor.href = 'https://github.com/vtwoptwo/portfolio';
+    
+
+          Object.assign(anchor.style, {
+            position: 'fixed',
+            boxSizing: 'border-box',
+            zIndex: '999',
+            bottom: '20px',
+            left: '20px',
+            color: 'black',
+            backgroundColor:'#22c55e',
+            padding: '5px 12px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: '11px',
+            fontWeight: '900',
+            border: '1px solid green',
+            boxShadow: '0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)',
+            transition: 'transform 150ms ease',
+            borderRadius: '20px',
+            textTransform: 'uppercase'
+
+            
+          
+            
+          });
+          // Setting hover effect via JavaScript
+          anchor.onmouseover = function() {
+            anchor.style.transform = 'translate(0,-2px)!important';
+          };
+    
+          anchor.onmouseout = function() {
+            anchor.style.transform = '';
+          };
         }
+      
+
       });
     });
 
